b49761f4ae4bb363d4ef85ba6b1bda966a001ace
Djapedjape123 <petafipedjaradic@gmail.com>
2026-04-11 00:34:23 +0200

    ajde deki

--- src/pages/Dogadjaj.jsx
+++ src/pages/Dogadjaj.jsx
@@ -61,7 +61,7 @@ function Dogadjaj() {
 
                 <div className="max-w-7xl mx-auto relative z-10">
                     
-                    {/* --- HERO SEKCIJA --- */}
+                    
                     <div className="text-center mb-16">
                         <motion.div
                             initial={{ opacity: 0, y: -20 }}
